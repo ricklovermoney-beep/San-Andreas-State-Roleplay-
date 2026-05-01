9a880d701e0f88b34ad1506a7a1187e6beb63fac
ricklovermoney-beep <ricklover.money@gmail.com>
2026-04-30 19:46:21 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Join San Andreas State Roleplay!",
         // Activity type number (0 = Playing).
         type: 0, 
       },
